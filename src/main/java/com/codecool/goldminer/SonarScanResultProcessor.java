502b160fe688c6d3fe8004540122b7b87ba40883
bakroli <bak.roland@hotmail.com>
2022-02-28 16:48:27 +0100

    partly

--- src/main/java/com/codecool/goldminer/SonarScanResultProcessor.java
+++ src/main/java/com/codecool/goldminer/SonarScanResultProcessor.java
@@ -17,17 +17,28 @@ public class SonarScanResultProcessor {
             e.printStackTrace();
         }
 
-        int goldCount = 0;
+        int goldCount = sumGold(rows);
+        int easyGoldCount = sumEasyGold(rows);
+
+        return new SurveyReport(easyGoldCount, goldCount);
+    }
+
+
+
+    private int sumGold(List<String> rows) {
+        int sum = 0;
         for (String row : rows) {
             for (char c : row.toCharArray()) {
                 if (c == 'g') {
-                    goldCount += 1;
+                    sum += 1;
                 }
             }
         }
+        return sum;
+    }
 
-        int easyGoldCount = 0;
-
+    private int sumEasyGold(List<String> rows) {
+        int sum = 0;
         for (String row : rows) {
             for (int i = 1; i < row.length() -1; i++) {
                 char c = row.charAt(i);
@@ -35,20 +46,11 @@ public class SonarScanResultProcessor {
                     char cUp = row.charAt(i+1);
                     char cDown = row.charAt(i-1);
                     if (cUp == ' ' || cDown == ' ') {
-                        easyGoldCount += 1;
+                        sum += 1;
                     }
                 }
             }
         }
-
-
-
-        SurveyReport gold = new SurveyReport(easyGoldCount, goldCount);
-
-
-
-
-
-        return gold;
+        return sum;
     }
 }
